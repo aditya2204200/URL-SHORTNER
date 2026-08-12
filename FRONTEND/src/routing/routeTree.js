9c04c91d93311ef58e2cd1b03e73c9b27f7f409a
Aditya Raj <ar25200312@gmail.com>
2026-08-12 22:58:14 +0530

    Fix import case for RootLayout component

--- FRONTEND/src/routing/routeTree.js
+++ FRONTEND/src/routing/routeTree.js
@@ -2,7 +2,7 @@ import { createRootRoute } from "@tanstack/react-router";
 import { homePageRoute } from "./homepage";
 import { authRoute } from "./auth.route";
 import { dashboardRoute } from "./dashboard";
-import RootLayout from "../rootLayout";
+import RootLayout from "../RootLayout";
 
 export const rootRoute = createRootRoute({
   component: RootLayout,
